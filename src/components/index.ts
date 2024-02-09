11227f3c13bad72d98ec89795b682f6238c68210
Dody Hartanto <muharridody@gmail.com>
2024-02-09 11:28:12 +0800

    feat: add text for about me sections

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as NavLink } from "./NavLink";
 export { default as Navbar } from "./Navbar";
 export { default as GeoMap } from "./GeoMap";
+export { default as AboutMe } from "./AboutMe";
