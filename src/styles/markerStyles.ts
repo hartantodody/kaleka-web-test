ff8ed433bdce6485025db8bc6d17cc9fd2677a9b
Dody Hartanto <muharridody@gmail.com>
2024-02-11 23:36:08 +0800

    feat: refactored styling from map component

---
+++ src/styles/markerStyles.ts
@@ -0,0 +1,9 @@
+import { CircleOptionsProps } from "../interfaces/interfaces";
+
+export const defaultColor: CircleOptionsProps = { color: "#478AF7", fillColor: "#D9D9D9", weight: 2 };
+
+const circleStyles = {
+  defaultColor,
+};
+
+export default circleStyles;
