af29c98740a59c6e2f2baf195e6400858635c898
Dody Hartanto <muharridody@gmail.com>
2024-02-11 23:34:33 +0800

    feat: add styling for public layout + add header text

--- src/layouts/PublicLayout/index.tsx
+++ src/layouts/PublicLayout/index.tsx
@@ -2,16 +2,19 @@ import { Navbar } from "../../components";
 import headerImage from "../../assets/header-farmer.webp";
 import { PublicLayoutProps } from "../../interfaces/interfaces";
 
-const PublicLayout = ({ children }: PublicLayoutProps) => {
+const PublicLayout = ({ children, headerText }: PublicLayoutProps) => {
   return (
     <>
-      <header>
+      <header className='relative'>
         <Navbar />
-        <div className='flex justify-center items-center h-[211px]'>
-          <img src={headerImage} className='h-full w-full object-cover' alt='farmer image' />
+        <div className='relative flex justify-center items-center pt-[103px]'>
+          <img src={headerImage} className='w-full object-cover h-[211px] ' alt='farmer image' />
+          <h1 className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[-50%] text-white text-[24px] lgtext-[32px] font-bold z-5 text-center'>
+            {headerText}
+          </h1>
         </div>
       </header>
-      <main>{children}</main>
+      <main className='flex flex-col justify-center items-center p-[15px] md:p-[68px]'>{children}</main>
     </>
   );
 };
